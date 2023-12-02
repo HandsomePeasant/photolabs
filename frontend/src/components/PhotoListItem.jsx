dd20c1aeae6bc9991f8c92527e7f6c6ab0a25330
Aden <aden.mccarville@gmail.com>
2023-12-02 01:38:23 -0500

    Implement PhotoList

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-
+import PhotoFavButton from './PhotoFavButton';
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({data}) => {
-  const { id, location, imageSource, username, profile } = data;
+  const { id, location, urls, user } = data;
+  const { full: imageSource, regular } = urls;
+  const { username, profile } = user;
 
   return (
   <div className="photo-list__item">
+    <PhotoFavButton />
     <img src={imageSource} className="photo-list__image" />
     <div className="photo-list__user-info">
       <img src={profile} className="photo-list__user-profile" />
