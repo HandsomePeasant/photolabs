e410eaefd1ea7405c99ac7d5914abeccb723e259
Aden <aden.mccarville@gmail.com>
2023-12-02 01:40:30 -0500

    Finalize Photolist to include a 'Like' button on each photo

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,21 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [selected, setSelected] = useState(false);
+  const [displayAlert, setDisplayAlert] = useState(false);
+
+  const handleClick = () => {
+    setSelected(!selected);
+    setDisplayAlert(true);
+    console.log(`User clicked the Like button`)
+  }
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={selected} displayAlert={displayAlert} />
       </div>
     </div>
   );
