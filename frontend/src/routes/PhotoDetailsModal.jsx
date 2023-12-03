c132f8b12c0b041eddad04c8c45df1ba99ce96f9
Aden <aden.mccarville@gmail.com>
2023-12-03 11:11:22 -0500

    Implement basic functionality of modal's close button

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import { useGlobalContext, closeModal } from '../App';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = () => {
+  const { state, dispatch } = useGlobalContext();
+
+  const handleClose = () => {
+    dispatch(closeModal());
+  };
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={handleClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
